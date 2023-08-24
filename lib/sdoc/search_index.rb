202a817c047a3434006668b8c4833ff4b9e60b90
Jonathan Hefner <jonathan@hefner.pro>
2023-08-24 01:22:39 -0500

    Rewrite search
    Prior to this commit, SDoc's search algorithm was implemented by
    [`searcher.js`][].  `searcher.js` builds a regular expression for each
    token in the query.  For example, the query "foo bar" generates the
    regular expressions `/([f])([^f]*?)([o])([^o]*?)([o])([^o]*?)/i` and
    `/([b])([^b]*?)([a])([^a]*?)([r])([^r]*?)/i`.  These regular expressions
    fuzzy match missing letters, but fail for any other kind of typo, such
    as added letters or swapped letters.  They can also produce surprising
    results.  For example, the query "ActiveRecord::Base" returns
    `ActiveRecord::AttributeAssignment` as the top result due to matching
    "activerecord" attri"b"ute"a"s"s"ignm"e"ent, and there are six(!) other
    results that appear before `ActiveRecord::Base`.
    
    This commit implements a new search algorithm based on character-level
    bigrams.  For example, the query "foo bar" will look for results that
    match "fo", "oo", "o ", " b", "ba", and "ar".  Shorthand bigrams for
    CamelCase names are also included in the search index.  For example,
    entries containing "ActiveRecord" are also associated with the bigram
    "ar".  Bigrams are weighted such that some contribute more to the match
    score, and results are ordered by match score.
    
    Here are some example queries and their top results with
    rails/rails@7c65a4b83b583f4f27f3f20a9fb078b35823d2fe both before and
    after this commit:
    
    * query: "ActiveRecord::Base"
      * top result before: `ActiveRecord::AttributeAssignment`
      * top result after: `ActiveRecord::Base`
    
    * query: "ar base"
      * top result before: `Rails::Generators::Testing::Behavior::ClassMethods#arguments`
      * top result after: `ActiveRecord::Base`
    
    * query: "has many"
      * top result before: `ActiveRecord::Associations::ClassMethods#has_and_belongs_to_many`
      * top result after: `ActiveRecord::Associations::ClassMethods#has_many`
    
    * query: "adress"
      * top result before: `ActiveSupport::HashWithIndifferentAccess`
      * top result after: `Mail::Address`
    
    * query: "existance"
      * top result before: no results
      * top result after: `Pathname#existence`
    
    * query: "foriegn"
      * top result before: no results
      * top result after: `String#foreign_key`
    
    This commit also redesigns the presentation of search results.  Prior to
    this commit, result names were cut off at ~43 characters, and result
    descriptions were cut off at ~53 characters.  And result descriptions
    included headings, further reducing relevant visible text.  For example,
    the description for `ActionCable::Connection::Base`, which has the
    heading "Action Cable Connection Base", was "Action Cable Connection
    Base For every WebSocket".  Result descriptions also included code
    blocks which were then mangled by `Searchdoc.Panel`'s `stripHTML`
    function.  For example, the description for `ActiveModel::API::new` was
    
      ```html
      <p>Initializes a new model with the given <code>params</code>.
    
      <pre><code>class Person
        include ActiveModel::API
        attr_accessor ...
      </code></pre>
      ```
    
    which was transformed to
    
      ```
      Initializes a new model with the given params.
    
      <codeclass Person
        include ActiveModel::API
        attr_accessor ...
      </pre
      ```
    
    With this commit, search results now always display the full name.
    Result descriptions are also fully displayed, including non-link HTML,
    and are now comprised of (up to) the first 130 characters of the leading
    paragraph of the RDoc comment.  For example, the description of
    `ActiveModel::API::new` becomes "Initializes a new model with the given
    <code>params</code>."
    
    [`searcher.js`]: https://github.com/ruby/rdoc/blob/v6.5.0/lib/rdoc/generator/template/json_index/js/searcher.js

---
+++ lib/sdoc/search_index.rb
@@ -0,0 +1,140 @@
+require "base64"
+require "nokogiri"
+
+module SDoc::SearchIndex
+  extend self
+
+  def generate(rdoc_modules)
+    # RDoc duplicates RDoc::MethodAttr instances when modules are aliased by
+    # assigning to a constant. For example, `MyBar = Foo::Bar` will duplicate
+    # all of Foo::Bar's RDoc::MethodAttr instances.
+    rdoc_objects = rdoc_modules + rdoc_modules.flat_map(&:method_list).uniq
+
+    bigram_sets = rdoc_objects.map { |rdoc_object| derive_bigrams(rdoc_object.full_name) }
+    bigram_bit_positions = compile_bigrams(bigram_sets)
+    bit_weights = compute_bit_weights(bigram_bit_positions)
+    entries = []
+
+    rdoc_objects.zip(bigram_sets) do |rdoc_object, bigrams|
+      entries << [
+        generate_fingerprint(bigrams, bigram_bit_positions), # Fingerprint
+        0.1 / Math.sqrt(rdoc_object.full_name.length), # Tie-breaker bonus per matching bigram
+        rdoc_object.path, # URL
+      ]
+
+      if rdoc_object.is_a?(RDoc::ClassModule)
+        entries.last << name_for(rdoc_object) # Class name
+        entries.last << nil # Method name
+      else
+        entries.last << name_for(rdoc_object.parent) # Class name
+        entries.last << name_for(rdoc_object) # Method name
+
+        # Reduce tie-breaker bonus in proportion to method name length. This
+        # prioritizes modules before methods, and short methods of long modules
+        # before long methods of short modules. For example, when searching for
+        # "find_by", this prioritizes ActiveRecord::FinderMethods#find_by before
+        # ActiveRecord::Querying#find_by_sql.
+        entries.last[1] *= 0.95 ** rdoc_object.name.length
+      end
+
+      if description = truncate_description(rdoc_object.description, 130)
+        entries.last << description # Summary
+      end
+    end
+
+    { "bigrams" => bigram_bit_positions, "weights" => bit_weights, "entries" => entries }
+  end
+
+  def derive_bigrams(name)
+    # Example: "ActiveSupport::Cache::Store#fetch" => [":ActiveSupport", ":Cache", ":Store", "#fetch"]
+    strings = ":#{name}".split(/:(?=:)|(?=#)/)
+    # Example: ":HashWithIndifferentAccess" => ":HWIA"
+    strings.concat(strings.map { |string| string.gsub(/([A-Z])[a-z]+/, '\1') })
+    # Example: ":HWIA" => ":hwia"
+    strings.concat(strings.map(&:downcase))
+    # Example: "#fetch_values" => "#fetchvalues"
+    strings.concat(strings.map { |string| string.tr("_", "") })
+    # Example: ":controller_name" => [" controller, " name"]
+    strings.concat(strings.flat_map { |string| string.tr(":#_", " ").split(/(?= )/) })
+
+    if method_name_first_char = name[/(?:#|::)([^A-Z])/, 1]
+      # Example: "ActionController::Metal::controller_name" => ".c"
+      strings << ".#{method_name_first_char}"
+      # Example: "ActionController::Metal::controller_name" => "e("
+      strings << "#{name[-1]}("
+    end
+
+    strings.flat_map { |string| string.each_char.each_cons(2).map(&:join) }.uniq
+  end
+
+  def compile_bigrams(bigram_sets)
+    # Assign each bigram a bit position based on its rarity. More common bigrams
+    # come first. This reduces the average number of bytes required to store a
+    # fingerprint.
+    bigram_sets.flatten.tally.sort_by(&:last).reverse.map(&:first).each_with_index.to_h
+  end
+
+  BIGRAM_PATTERN_WEIGHTS = {
+    /[^a-z]/ => 2, # Bonus point for non-lowercase-alpha chars because they show intentionality.
+    /^ / => 3, # More points for matching start of token because it shows more intentionality.
+    /^:/ => 4, # Slightly more points for start of module because it shows even more intentionality.
+    /[#.(]/ => 50, # Strongly prefer methods when query includes "#", ".", or "(".
+  }
+
+  def compute_bit_weights(bigram_bit_positions)
+    bigram_bit_positions.uniq(&:last).sort_by(&:last).map do |bigram, _position|
+      BIGRAM_PATTERN_WEIGHTS.map { |pattern, weight| bigram.match?(pattern) ? weight : 1 }.max
+    end
+  end
+
+  def generate_fingerprint(bigrams, bigram_bit_positions)
+    bit_positions = bigrams.map(&bigram_bit_positions)
+    byte_count = ((bit_positions.max + 1) / 8.0).ceil
+    bytes = [0] * byte_count
+
+    bit_positions.each do |position|
+      bytes[position / 8] |= 1 << (position % 8)
+    end
+
+    bytes
+  end
+
+  def name_for(rdoc_object)
+    case rdoc_object
+    when RDoc::MethodAttr
+      sigil = rdoc_object.singleton ? "::" : "#"
+      params = rdoc_object.call_seq ? "(...)" : rdoc_object.params
+      "#{sigil}#{rdoc_object.name}#{params}"
+    else
+      rdoc_object.full_name
+    end
+  end
+
+  def truncate_description(description, limit)
+    return if description.empty?
+    leading_paragraph = Nokogiri::HTML.fragment(description).at_css("h1 + p, p:first-child")
+    return unless leading_paragraph
+
+    # Replace links with their inner HTML
+    leading_paragraph.css("a").each { |a| a.replace(a.children) }
+
+    # Truncate text (but not in <code> elements)
+    leading_paragraph.traverse do |node|
+      if limit <= 0
+        node.remove if node.children.empty?
+      elsif node.text?
+        limit -= node.content.length
+        if limit < 0 && node.parent.name != "code"
+          node.content = node.content[0..limit].sub(/(?:\W+|\W*\w+)\Z/, "")
+        end
+      end
+    end
+
+    # Append ellipsis if truncated or if paragraph refers to a subsequent block
+    if limit < 0 || leading_paragraph.inner_text.end_with?(":")
+      leading_paragraph.add_child("...")
+    end
+
+    leading_paragraph.inner_html
+  end
+end
